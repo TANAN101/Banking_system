461c38338f96b4557957490677eca329e58cdc6e
TANAN <tanan@TANANs-MacBook-Air.local>
2026-04-27 00:26:25 +0300

    update

---
+++ src/Banking_system.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+    }
+    class Student {
+       public int age=20;
+        public String name="tanan"; // instance variable private int age;
+
+        public void display() {
+            System.out.println(name + ", " + age);
+        }
+    }
+}
